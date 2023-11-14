683fa15b7ba1860f0bd9e927e21888a260aeab67
youssef wafik <youssef.wafik@cyshield.com>
2023-11-14 20:52:28 +0200

    register form

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,10 +1,22 @@
-import { Component } from "@angular/core"
+import {Component} from '@angular/core'
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms'
 
 @Component({
-    selector: 'mc-register',
-    templateUrl: './register.component.html',
-    standalone: true
+  selector: 'mc-register',
+  templateUrl: './register.component.html',
+  standalone: true,
+  imports: [ReactiveFormsModule],
 })
 export class RegisterComponent {
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  })
 
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit() {
+    console.log('form', this.form.getRawValue())
+  }
 }
